36b6c14f072a6f8eb973023f00d7b8b7e7457cc9
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-12-31 16:34:16 +0530

    added

--- DynamicProgramming/dp_maxRodCost.cpp
+++ DynamicProgramming/dp_maxRodCost.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 ////////////By Recursion/////////////////
+/**
 int Revenue(int n, vector<int>& prices){
 
 if(n==0){
@@ -20,9 +21,25 @@ for(int i=0;i<n;++i){
 return max;
 
 }
-
+**/
 /////////By DP/////////////////////////
 
+int Revenue(int n, vector<int>& prices){
+
+vector<int> R(n+1);
+R[0]=0;
+for(int i=1;i<=n;++i){
+    int max=-1;
+    for(int j=1;j<=i;++j){
+        int temp = prices[j-1] + R[i-j];
+        if(temp>max){
+            max=temp;
+        }
+    }
+    R[i]=max;
+}
+return R[n];
+}
 
 int main() {
 
